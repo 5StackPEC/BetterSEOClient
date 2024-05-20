944d23a1c8e19b71153d6960d80a43a8463d44ee
sant-gdc <83039994+sant-gdc@users.noreply.github.com>
2024-05-20 13:02:40 -0600

    File drag/search and upload and button with alert working

--- BetterSEOClient/src/App.tsx
+++ BetterSEOClient/src/App.tsx
@@ -1,34 +1,19 @@
 import { useState } from 'react'
-import { IconContext } from "react-icons"
-import { FiUpload } from "react-icons/fi"
-import { FileUploader } from "react-drag-drop-files"
-import DragNdrop from "./components/dragAndDrop"
+import Dropzone from './components/dragAndDrop'
 import './App.css'
-
-const fileTypes = ["JPEG", "PNG", "JPG"];
 
 function App() {
   const [count, setCount] = useState(0)
-  const [file, setFile] = useState(null)
-  const handleChange = (file:any) => {
-    setFile(file);
+  function handleClick() {
+    alert('File uploaded!');
   }
-
+  
   return (
     <>
       <h1>BetterSEO</h1>
       <p>Take your website to the next level</p>
-      <div className="card">
-        <IconContext.Provider value={{ color: "white", size: '80px' }}>
-          <div>
-            <FiUpload />
-          </div>
-        </IconContext.Provider>
-        <h2>Drag and drop files or browse</h2>
-        <p>Supported formats: PNG, JPG, JPEG</p>
-      </div>
-      <h2>Upload your screenshot, we will handle it.</h2>
-      <button>Upload Screenshot</button>
+      <Dropzone />
+      <button onClick={handleClick}>Upload Screenshot</button>
     </>
   )
 }
--- BetterSEOClient/src/components/dragAndDrop.tsx
+++ BetterSEOClient/src/components/dragAndDrop.tsx
@@ -1,61 +1,35 @@
-import { DragEvent, useState } from "react";
+import { useDropzone, FileWithPath } from "React-dropzone"
+import { IconContext } from "react-icons"
+import { FiUpload } from "react-icons/fi"
+import '../style/Dropzone.css'
 
-export default function FileDrop() {
-  const [isOver, setIsOver] = useState(false);
-  const [files, setFiles] = useState<File[]>([]);
+export default function Dropzone() {
+  const { getRootProps, getInputProps, acceptedFiles } = useDropzone({})
 
-  // Define the event handlers
-  const handleDragOver = (event: DragEvent<HTMLDivElement>) => {
-    event.preventDefault();
-    setIsOver(true);
-  };
-
-  const handleDragLeave = (event: DragEvent<HTMLDivElement>) => {
-    event.preventDefault();
-    setIsOver(false);
-  };
-
-  const handleDrop = (event: DragEvent<HTMLDivElement>) => {
-    event.preventDefault();
-    setIsOver(false);
-
-    // Fetch the files
-    const droppedFiles = Array.from(event.dataTransfer.files);
-    setFiles(droppedFiles);
-
-    // Use FileReader to read file content
-    droppedFiles.forEach((file) => {
-      const reader = new FileReader();
-
-      reader.onloadend = () => {
-        console.log(reader.result);
-      };
-
-      reader.onerror = () => {
-        console.error("There was an issue reading the file.");
-      };
-
-      reader.readAsDataURL(file);
-      return reader;
-    });
-  };
+  const files = acceptedFiles.map((file: FileWithPath) => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+    </li>
+  ))
 
   return (
-    <div
-      onDragOver={handleDragOver}
-      onDragLeave={handleDragLeave}
-      onDrop={handleDrop}
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "50px",
-        width: "300px",
-        border: "1px dotted",
-        backgroundColor: isOver ? "lightgray" : "white",
-      }}
-    >
-      Drag and drop some files here
+    <div {...getRootProps({ className: "dropzone" })}>
+      <input className="input-zone" {...getInputProps()} />
+      <div className="dropzone-content">
+        <div className="card"> 
+          <IconContext.Provider value={{ color: "white", size: '80px' }}>
+            <div>
+              <FiUpload />
+            </div>
+          </IconContext.Provider>
+          <h2 >Drag and drop files or browse</h2>
+          <p>Supported formats: PNG, JPG, JPEG</p>
+        </div>
+      </div>
+      <h2>Upload your screenshot, we will handle it.</h2>
+      <aside>
+        <p className="file">{files}</p>
+      </aside>
     </div>
-  );
+  )
 }
