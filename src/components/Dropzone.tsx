0b16c5ad4e5c620e8ab74aafb5add916ae38e77f
CristianCazares <cm_cjavier@hotmail.com>
2024-05-30 01:04:40 -0600

    feat(Dropzone): implemented onDrop function

--- src/components/Dropzone.tsx
+++ src/components/Dropzone.tsx
@@ -1,15 +1,29 @@
+import { useState } from "react";
 import { useDropzone, FileWithPath } from "React-dropzone";
 import { IconContext } from "react-icons";
 import { FiUpload } from "react-icons/fi";
 import "../style/Dropzone.css";
 
-export default function Dropzone() {
+interface Image {
+  file: File;
+  preview: string;
+}
+
+const Dropzone = () => {
+  const [image, setImage] = useState<Image>();
+
+  const onDrop = (acceptedFiles: File[]) => {
+    const file = acceptedFiles[0];
+    setImage({ file: file, preview: URL.createObjectURL(file) });
+  };
+
   const { getRootProps, getInputProps, acceptedFiles } = useDropzone({
     multiple: false,
     accept: {
       "image/png": [".png"],
       "image/jpg": [".jpeg", ".jpg"],
     },
+    onDrop,
   });
 
   const files = acceptedFiles.map((file: FileWithPath) => (
@@ -29,15 +43,18 @@ export default function Dropzone() {
             </IconContext.Provider>
             <h2>Drag and drop files or browse</h2>
             <p>Supported formats: PNG, JPG, JPEG</p>
+            {files.length !== 0 && <img src={image?.preview} className="imagePreview" />}
           </div>
         </div>
       </div>
       <div>
         <h2>Upload your screenshot, we will handle it.</h2>
         <aside>
-          <p className="file">{files}</p>
+          {files.length !== 0 ? <p className="file">{files}</p> : <p>&nbsp;</p>}
         </aside>
       </div>
     </>
   );
-}
+};
+
+export default Dropzone;
