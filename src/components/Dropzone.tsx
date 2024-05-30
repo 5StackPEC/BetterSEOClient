46b42f136f590bc6bc89f54f8c79a62b9a345835
CristianCazares <cm_cjavier@hotmail.com>
2024-05-30 00:05:15 -0600

    fix(Dropzone): adjusted dropzone to not include extra items

--- src/components/Dropzone.tsx
+++ src/components/Dropzone.tsx
@@ -1,35 +1,37 @@
-import { useDropzone, FileWithPath } from "React-dropzone"
-import { IconContext } from "react-icons"
-import { FiUpload } from "react-icons/fi"
-import '../style/Dropzone.css'
+import { useDropzone, FileWithPath } from "React-dropzone";
+import { IconContext } from "react-icons";
+import { FiUpload } from "react-icons/fi";
+import "../style/Dropzone.css";
 
 export default function Dropzone() {
-  const { getRootProps, getInputProps, acceptedFiles } = useDropzone({})
+  const { getRootProps, getInputProps, acceptedFiles } = useDropzone({});
 
   const files = acceptedFiles.map((file: FileWithPath) => (
     <li key={file.path}>
       {file.path} - {file.size} bytes
     </li>
-  ))
+  ));
 
   return (
-    <div {...getRootProps({ className: "dropzone" })}>
-      <input className="input-zone" {...getInputProps()} />
-      <div className="dropzone-content">
-        <div className="card"> 
-          <IconContext.Provider value={{ color: "white", size: '80px' }}>
-            <div>
+    <>
+      <div {...getRootProps({ className: "dropzone" })}>
+        <input className="input-zone" {...getInputProps()} />
+        <div className="dropzone-content">
+          <div className="card">
+            <IconContext.Provider value={{ color: "white", size: "80px" }}>
               <FiUpload />
-            </div>
-          </IconContext.Provider>
-          <h2 >Drag and drop files or browse</h2>
-          <p>Supported formats: PNG, JPG, JPEG</p>
+            </IconContext.Provider>
+            <h2>Drag and drop files or browse</h2>
+            <p>Supported formats: PNG, JPG, JPEG</p>
+          </div>
         </div>
       </div>
-      <h2>Upload your screenshot, we will handle it.</h2>
-      <aside>
-        <p className="file">{files}</p>
-      </aside>
-    </div>
-  )
+      <div>
+        <h2>Upload your screenshot, we will handle it.</h2>
+        <aside>
+          <p className="file">{files}</p>
+        </aside>
+      </div>
+    </>
+  );
 }
